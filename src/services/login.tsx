e98c00fbf51ee4d2916dbc02b5fcd1133479c9a4
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-11-14 22:28:22 -0300

    update project

--- src/services/login.tsx
+++ src/services/login.tsx
@@ -1,17 +1,5 @@
-import { ILogin } from "@/interfaces/ILogin";
-
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
-// export const logIn = async (login: ILogin) => {
-//     return await fetch(`${API_URL}/login`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(login)
-//     });
-// }
-
-export const logIn = async (login: ILogin): Promise<Response> => {
+export const logIn = async () => {
     return await fetch(`${API_URL}/login`);
 }
